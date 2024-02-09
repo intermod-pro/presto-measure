e97e27a9be900e153a7ba2c252e24c14050c4faa
marina <125870415+Marina-IMP@users.noreply.github.com>
2024-02-09 14:23:41 +0100

    added square rabi_amp and square memory_sweep with ability to change time between the starts of the memory and control pulses

--- rabi_amp_square.py
+++ rabi_amp_square.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
-"""Measure the frequency of the memory."""
-from typing import Optional
+"""
+Measure Rabi oscillation by changing the amplitude of the control pulse.
+
+The control pulse has a square envelope, while the readout pulse is square.
+"""
+import ast
+import math
+from typing import List, Optional, Tuple, Union
 
 import h5py
 import numpy as np
@@ -8,7 +14,7 @@ import numpy.typing as npt
 
 from presto.hardware import AdcMode, DacMode
 from presto import pulsed
-from presto.utils import plot_sequence, rotate_opt, sin2
+from presto.utils import format_precision, rotate_opt, sin2
 
 from _base import Base
 
@@ -17,54 +23,46 @@ IDX_LOW = 0
 IDX_HIGH = -1
 
 
-class Sweep_memory(Base):
+class RabiAmp(Base):
     def __init__(
         self,
         readout_freq: float,
         control_freq: float,
-        memory_freq_center: float,
-        memory_freq_span: float,
-        memory_freq_nr: int,
         readout_amp: float,
-        control_amp: float,
-        memory_amp: float,
+        control_amp_arr: Union[List[float], npt.NDArray[np.float64]],
         readout_duration: float,
         control_duration: float,
-        memory_duration: float,
         sample_duration: float,
         readout_port: int,
         control_port: int,
-        memory_port: int,
         sample_port: int,
         wait_delay: float,
         readout_sample_delay: float,
         num_averages: int,
-        dt: float = 0,  # delay between start of memory and start of control pulse
+        num_pulses: int = 1,
+        jpa_params: Optional[dict] = None,
+        drag: float = 0.0,
     ) -> None:
         self.readout_freq = readout_freq
         self.control_freq = control_freq
-        self.memory_freq_center = memory_freq_center
-        self.memory_freq_span = memory_freq_span
-        self.memory_freq_nr = memory_freq_nr
         self.readout_amp = readout_amp
-        self.control_amp = control_amp
-        self.memory_amp = memory_amp
+        self.control_amp_arr = np.atleast_1d(control_amp_arr).astype(np.float64)
         self.readout_duration = readout_duration
         self.control_duration = control_duration
-        self.memory_duration = memory_duration
         self.sample_duration = sample_duration
         self.readout_port = readout_port
         self.control_port = control_port
-        self.memory_port = memory_port
         self.sample_port = sample_port
         self.wait_delay = wait_delay
         self.readout_sample_delay = readout_sample_delay
         self.num_averages = num_averages
-        self.dt = dt
+        self.num_pulses = num_pulses
+        self.drag = drag
 
         self.t_arr = None  # replaced by run
         self.store_arr = None  # replaced by run
-        self.memory_freq_arr = None  # replaced by run
+
+        self.jpa_params = jpa_params
 
     def run(
         self,
@@ -77,25 +75,25 @@ class Sweep_memory(Base):
             address=presto_address,
             port=presto_port,
             ext_ref_clk=ext_ref_clk,
-            dry_run=False,
             adc_mode=AdcMode.Mixed,
             dac_mode=DacMode.Mixed,
         ) as pls:
             pls.hardware.set_adc_attenuation(self.sample_port, 0.0)
             pls.hardware.set_dac_current(self.readout_port, DAC_CURRENT)
             pls.hardware.set_dac_current(self.control_port, DAC_CURRENT)
-            pls.hardware.set_dac_current(self.memory_port, DAC_CURRENT)
             pls.hardware.set_inv_sinc(self.readout_port, 0)
             pls.hardware.set_inv_sinc(self.control_port, 0)
-            pls.hardware.set_inv_sinc(self.memory_port, 0)
 
             pls.hardware.configure_mixer(
                 self.readout_freq,
                 in_ports=self.sample_port,
                 out_ports=self.readout_port,
-            )
-            pls.hardware.configure_mixer(self.control_freq, out_ports=self.control_port)
-            pls.hardware.configure_mixer(self.memory_freq_center, out_ports=self.memory_port)
+                sync=False,
+            )  # sync in next call
+
+            pls.hardware.configure_mixer(
+                self.control_freq, out_ports=self.control_port, sync=True
+            )  # sync here
 
             # ************************************
             # *** Setup measurement parameters ***
@@ -103,21 +101,8 @@ class Sweep_memory(Base):
 
             # Setup lookup tables for amplitudes
             pls.setup_scale_lut(self.readout_port, group=0, scales=self.readout_amp)
-            pls.setup_scale_lut(self.control_port, group=0, scales=self.control_amp)
-            pls.setup_scale_lut(self.memory_port, group=0, scales=self.memory_amp)
+            pls.setup_scale_lut(self.control_port, group=0, scales=self.control_amp_arr)
 
-            # Setup lookup tables for memory frequency
-            self.memory_freq_arr = self.memory_freq_center + np.linspace(
-                -self.memory_freq_span / 2, self.memory_freq_span / 2, self.memory_freq_nr
-            )
-            memory_if_arr = self.memory_freq_arr - self.memory_freq_center
-            mask = memory_if_arr < 0
-            memory_if_arr = np.abs(memory_if_arr)
-            ph_i = np.zeros_like(memory_if_arr)
-            ph_q = ph_i - np.pi / 2 + mask * np.pi  # +-np.pi/2 for low/high sideband
-            pls.setup_freq_lut(
-                self.memory_port, group=0, frequencies=memory_if_arr, phases=ph_i, phases_q=ph_q
-            )
             # Setup readout and control pulses
             # use setup_long_drive to create a pulse with square envelope
             # setup_long_drive supports smooth rise and fall transitions for the pulse,
@@ -130,21 +115,13 @@ class Sweep_memory(Base):
                 envelope=False,
             )
 
-            # number of samples in the control template
+            # For the control pulse we create a squared envelope,
             control_pulse = pls.setup_long_drive(
                 self.control_port,
                 group=0,
                 duration=self.control_duration,
                 amplitude=1.0 + 1j,
                 envelope=False,
-            )
-
-            memory_pulse = pls.setup_long_drive(
-                self.memory_port,
-                group=0,
-                duration=self.memory_duration,
-                amplitude=1.0 + 1j,
-                envelope=True,
             )
 
             # Setup sampling window
@@ -155,21 +132,23 @@ class Sweep_memory(Base):
             # *** Program pulse sequence ***
             # ******************************
             T = 0.0  # s, start at time zero ...
-            pls.reset_phase(T, self.memory_port, group=0)
-            pls.output_pulse(T, memory_pulse)  # displace memory
-            T += self.dt
-            pls.output_pulse(T, control_pulse)  # control conditional on memory being |0>
-            T += control_pulse.get_duration()
+            for _ in range(self.num_pulses):
+                pls.output_pulse(T, control_pulse)  # Control pulse
+                T += self.control_duration
             pls.output_pulse(T, readout_pulse)  # Readout
             pls.store(T + self.readout_sample_delay)
-            T += readout_pulse.get_duration()
-            pls.next_frequency(T, self.memory_port, group=0)
+            T += self.readout_duration
+            pls.next_scale(T, self.control_port)  # Move to next Rabi amplitude
             T += self.wait_delay  # Wait for decay
 
             # **************************
             # *** Run the experiment ***
             # **************************
-            pls.run(period=T, repeat_count=self.memory_freq_nr, num_averages=self.num_averages)
+            # repeat the whole sequence `nr_amps` times
+            # then average `num_averages` times
+
+            nr_amps = len(self.control_amp_arr)
+            pls.run(period=T, repeat_count=nr_amps, num_averages=self.num_averages)
             self.t_arr, self.store_arr = pls.get_store_data()
 
         return self.save()
@@ -178,67 +157,63 @@ class Sweep_memory(Base):
         return super()._save(__file__, save_filename=save_filename)
 
     @classmethod
-    def load(cls, load_filename: str) -> "Sweep_memory":
+    def load(cls, load_filename: str) -> "RabiAmp":
         with h5py.File(load_filename, "r") as h5f:
             readout_freq = float(h5f.attrs["readout_freq"])  # type: ignore
             control_freq = float(h5f.attrs["control_freq"])  # type: ignore
-            memory_freq_center = float(h5f.attrs["memory_freq_center"])  # type: ignore
-            memory_freq_span = float(h5f.attrs["memory_freq_span"])  # type: ignore
-            memory_freq_nr = int(h5f.attrs["memory_freq_nr"])  # type: ignore
             readout_amp = float(h5f.attrs["readout_amp"])  # type: ignore
-            control_amp = float(h5f.attrs["control_amp"])  # type: ignore
-            memory_amp = float(h5f.attrs["memory_amp"])  # type: ignore
+            control_amp_arr: npt.NDArray[np.float64] = h5f["control_amp_arr"][()]  # type: ignore
             readout_duration = float(h5f.attrs["readout_duration"])  # type: ignore
             control_duration = float(h5f.attrs["control_duration"])  # type: ignore
-            memory_duration = float(h5f.attrs["memory_duration"])  # type: ignore
             sample_duration = float(h5f.attrs["sample_duration"])  # type: ignore
             readout_port = int(h5f.attrs["readout_port"])  # type: ignore
             control_port = int(h5f.attrs["control_port"])  # type: ignore
-            memory_port = int(h5f.attrs["memory_port"])  # type: ignore
             sample_port = int(h5f.attrs["sample_port"])  # type: ignore
             wait_delay = float(h5f.attrs["wait_delay"])  # type: ignore
             readout_sample_delay = float(h5f.attrs["readout_sample_delay"])  # type: ignore
             num_averages = int(h5f.attrs["num_averages"])  # type: ignore
-            dt = float(h5f.attrs["dt"])  # type: ignore
+            num_pulses = int(h5f.attrs["num_pulses"])  # type: ignore
 
-            t_arr: npt.NDArray[np.float64] = h5f["t_arr"][()]  # type:ignore
-            store_arr: npt.NDArray[np.complex128] = h5f["store_arr"][()]  # type:ignore
-            memory_freq_arr: npt.NDArray[np.float64] = h5f["memory_freq_arr"][()]  # type:ignore
+            jpa_params: dict = ast.literal_eval(h5f.attrs["jpa_params"])  # type: ignore
+
+            t_arr: npt.NDArray[np.float64] = h5f["t_arr"][()]  # type: ignore
+            store_arr: npt.NDArray[np.complex128] = h5f["store_arr"][()]  # type: ignore
+
+            try:
+                drag = float(h5f.attrs["drag"])  # type: ignore
+            except KeyError:
+                drag = 0.0
 
         self = cls(
             readout_freq=readout_freq,
             control_freq=control_freq,
-            memory_freq_center=memory_freq_center,
-            memory_freq_span=memory_freq_span,
-            memory_freq_nr=memory_freq_nr,
             readout_amp=readout_amp,
-            control_amp=control_amp,
-            memory_amp=memory_amp,
+            control_amp_arr=control_amp_arr,
             readout_duration=readout_duration,
             control_duration=control_duration,
-            memory_duration=memory_duration,
             sample_duration=sample_duration,
             readout_port=readout_port,
             control_port=control_port,
-            memory_port=memory_port,
             sample_port=sample_port,
             wait_delay=wait_delay,
             readout_sample_delay=readout_sample_delay,
             num_averages=num_averages,
-            dt=dt,
+            num_pulses=num_pulses,
+            jpa_params=jpa_params,
+            drag=drag,
         )
         self.t_arr = t_arr
         self.store_arr = store_arr
-        self.memory_freq_arr = memory_freq_arr
+
         return self
 
     def analyze(self, all_plots: bool = False):
-        assert self.t_arr is not None
-        assert self.store_arr is not None
-        assert self.memory_freq_arr is not None
+        if self.t_arr is None:
+            raise RuntimeError
+        if self.store_arr is None:
+            raise RuntimeError
 
         import matplotlib.pyplot as plt
-        from scipy.optimize import curve_fit
 
         ret_fig = []
         t_low = self.t_arr[IDX_LOW]
@@ -253,12 +228,45 @@ class Sweep_memory(Base):
             ax11.plot(1e9 * self.t_arr, np.abs(self.store_arr[0, 0, :]))
             ax12.plot(1e9 * self.t_arr, np.angle(self.store_arr[0, 0, :]))
             ax12.set_xlabel("Time [ns]")
-            fig1.show()  # type: ignore
+            fig1.show()
             ret_fig.append(fig1)
 
-        # Analyze
+        # Analyze Rabi
         resp_arr = np.mean(self.store_arr[:, 0, IDX_LOW:IDX_HIGH], axis=-1)
         data = rotate_opt(resp_arr)
+
+        # Fit data
+        popt_x, perr_x = _fit_period(self.control_amp_arr, np.real(data))
+        period = popt_x[3] * self.num_pulses
+        period_err = perr_x[3] * self.num_pulses
+        pi_amp = period / 2
+        pi_2_amp = period / 4
+        if self.num_pulses > 1:
+            print(f"{self.num_pulses} pulses")
+
+        print(f"Tau pulse amplitude: {format_precision(period, period_err)} FS")
+        print(f"Pi pulse amplitude: {format_precision(pi_amp, period_err / 2)} FS")
+        print(f"Pi/2 pulse amplitude: {format_precision(pi_2_amp, period_err / 4)} FS")
+
+        print(f"control_amp_180 = {pi_amp:.5f}")
+        print(f"control_amp_90 = {pi_2_amp:.5f}")
+
+        if all_plots:
+            fig2, ax2 = plt.subplots(4, 1, sharex=True, figsize=(6.4, 6.4), tight_layout=True)
+            ax21, ax22, ax23, ax24 = ax2
+            ax21.plot(self.control_amp_arr, np.abs(data))
+            ax22.plot(self.control_amp_arr, np.angle(data))
+            ax23.plot(self.control_amp_arr, np.real(data))
+            ax23.plot(self.control_amp_arr, _func(self.control_amp_arr, *popt_x), "--")
+            ax24.plot(self.control_amp_arr, np.imag(data))
+
+            ax21.set_ylabel("Amplitude [FS]")
+            ax22.set_ylabel("Phase [rad]")
+            ax23.set_ylabel("I [FS]")
+            ax24.set_ylabel("Q [FS]")
+            ax2[-1].set_xlabel("Pulse amplitude [FS]")
+            fig2.show()
+            ret_fig.append(fig2)
 
         data_max = np.abs(data).max()
         unit = ""
@@ -273,36 +281,53 @@ class Sweep_memory(Base):
             unit = "m"
             mult = 1e3
 
-        fig2, ax2 = plt.subplots(4, 1, sharex=True, figsize=(6.4, 6.4), tight_layout=True)
-        ax21, ax22, ax23, ax24 = ax2
-        ax21.plot(1e-9 * self.memory_freq_arr, mult * np.abs(data))
-        ax22.plot(1e-9 * self.memory_freq_arr, np.angle(data))
-        ax23.plot(1e-9 * self.memory_freq_arr, mult * np.real(data))
-        try:
-            data_min = data.real.min()
-            data_max = data.real.max()
-            data_rng = data_max - data_min
-            p0 = [self.memory_freq_center, self.memory_freq_span / 4, data_rng, data_min]
-            popt, _ = curve_fit(_gaussian, self.memory_freq_arr, data.real, p0)
-            ax23.plot(
-                1e-9 * self.memory_freq_arr, mult * _gaussian(self.memory_freq_arr, *popt), "--"
-            )
-            print(f"f0 = {popt[0]} Hz")
-            print(f"sigma = {abs(popt[1])} Hz")
-        except Exception:
-            print("fit failed")
-        ax24.plot(1e-9 * self.memory_freq_arr, mult * np.imag(data))
-
-        ax21.set_ylabel(f"Amplitude [{unit:s}FS]")
-        ax22.set_ylabel("Phase [rad]")
-        ax23.set_ylabel(f"I [{unit:s}FS]")
-        ax24.set_ylabel(f"Q [{unit:s}FS]")
-        ax2[-1].set_xlabel("Control frequency [GHz]")
-        fig2.show()
-        ret_fig.append(fig2)
+        fig3, ax3 = plt.subplots(tight_layout=True)
+        ax3.plot(self.control_amp_arr, mult * np.real(data), ".")
+        ax3.plot(self.control_amp_arr, mult * _func(self.control_amp_arr, *popt_x), "--")
+        ax3.set_ylabel(f"I quadrature [{unit:s}FS]")
+        ax3.set_xlabel("Pulse amplitude [FS]")
+        if self.num_pulses > 1:
+            ax3.set_title(f"{self.num_pulses} pulses")
+        # fig3.show()
+        ret_fig.append(fig3)
 
         return ret_fig
 
 
-def _gaussian(x, x0, s, a, o):
-    return a * np.exp(-0.5 * ((x - x0) / s) ** 2) + o
+def _func(t, offset, amplitude, T2, period, phase):
+    frequency = 1 / period
+    return offset + amplitude * np.exp(-t / T2) * np.cos(math.tau * frequency * t + phase)
+
+
+def _fit_period(
+    x: npt.NDArray[np.float64], y: npt.NDArray[np.float64]
+) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+    from scipy.optimize import curve_fit
+
+    pkpk = np.max(y) - np.min(y)
+    offset = np.min(y) + pkpk / 2
+    amplitude = 0.5 * pkpk
+    T2 = 0.5 * (np.max(x) - np.min(x))
+    freqs = np.fft.rfftfreq(len(x), x[1] - x[0])
+    fft = np.fft.rfft(y)
+    frequency = freqs[1 + np.argmax(np.abs(fft[1:]))]
+    period = 1 / frequency
+    first = (y[0] - offset) / amplitude
+    if first > 1.0:
+        first = 1.0
+    elif first < -1.0:
+        first = -1.0
+    phase = np.arccos(first)
+    p0 = (
+        offset,
+        amplitude,
+        T2,
+        period,
+        phase,
+    )
+    res = curve_fit(_func, x, y, p0=p0)
+    popt = res[0]
+    pcov = res[1]
+    perr = np.sqrt(np.diag(pcov))
+    offset, amplitude, T2, period, phase = popt
+    return popt, perr
